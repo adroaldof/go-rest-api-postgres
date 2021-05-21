5508203f3e446f7edd721d33e1041b41ef618f8e
Adroaldo de Andrade <adroaldof@gmail.com>
2021-05-21 19:35:32 -0300

    Add article details

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 type Article struct {
-	// ID  string `json:"ID"`
+	ID     string `json:"ID"`
 	Title  string `json:"Title"`
 	Author string `json:"Author"`
 	Link   string `json:"Link"`
@@ -31,25 +31,19 @@ func homePage(response http.ResponseWriter, request *http.Request) {
 }
 
 func getArticles(response http.ResponseWriter, request *http.Request) {
-	Articles = []Article{
-		{
-			Title:  "Python Intermediate and Advanced 101",
-			Author: "Arkaprabha Majumdar",
-			Link:   "https://www.amazon.com/dp/B089KVK23P",
-		},
-		{
-			Title:  "R programming Advanced",
-			Author: "Arkaprabha Majumdar",
-			Link:   "https://www.amazon.com/dp/B089WH12CR",
-		},
-		{
-			Title:  "R programming Fundamentals",
-			Author: "Arkaprabha Majumdar",
-			Link:   "https://www.amazon.com/dp/B089S58WWG",
-		},
+	json.NewEncoder(response).Encode(Articles)
+}
+
+func getArticle(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	id := vars["id"]
+
+	for _, article := range Articles {
+		if article.ID == id {
+			json.NewEncoder(response).Encode(article)
+		}
 	}
 
-	json.NewEncoder(response).Encode(Articles)
 }
 
 func handleRequests() {
@@ -57,10 +51,32 @@ func handleRequests() {
 
 	router.HandleFunc("/", homePage)
 	router.HandleFunc("/articles", getArticles)
+	router.HandleFunc("/articles/{id}", getArticle)
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func main() {
+	Articles = []Article{
+		{
+			ID:     "1",
+			Title:  "Python Intermediate and Advanced 101",
+			Author: "Arkaprabha Majumdar",
+			Link:   "https://www.amazon.com/dp/B089KVK23P",
+		},
+		{
+			ID:     "2",
+			Title:  "R programming Advanced",
+			Author: "Arkaprabha Majumdar",
+			Link:   "https://www.amazon.com/dp/B089WH12CR",
+		},
+		{
+			ID:     "3",
+			Title:  "R programming Fundamentals",
+			Author: "Arkaprabha Majumdar",
+			Link:   "https://www.amazon.com/dp/B089S58WWG",
+		},
+	}
+
 	handleRequests()
 }
