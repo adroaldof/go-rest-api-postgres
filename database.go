276e486053d67fde22a084db22e7a72995e7b595
Adroaldo de Andrade <adroaldof@gmail.com>
2021-05-23 10:18:55 -0300

    Allow database to get the environment variable

--- database.go
+++ database.go
@@ -8,24 +8,17 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-const (
-	DBUser     = "user"
-	DBPassword = "password"
-	DBName     = "go_api_db"
-	DBHost     = "0.0.0.0"
-	DBPort     = "5432"
-	DBType     = "postgres"
-)
-
 func getPostgresConnectionString() string {
-	dataBase := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
-		DBHost,
-		DBPort,
-		DBUser,
-		DBName,
-		DBPassword)
+	databaseUrlPattern := "host=%s port=%s user=%s dbname=%s password=%s sslmode=disable"
 
-	return dataBase
+	databaseUrl := fmt.Sprintf(databaseUrlPattern,
+		GetEnvironmentVariable("POSTGRES_HOST", "0.0.0.0"),
+		GetEnvironmentVariable("POSTGRES_PORT", "5432"),
+		GetEnvironmentVariable("POSTGRES_USER", "user"),
+		GetEnvironmentVariable("POSTGRES_NAME", "go_api_db"),
+		GetEnvironmentVariable("POSTGRES_PASSWORD", "password"))
+
+	return databaseUrl
 }
 
 func GetDBInstance() *gorm.DB {
